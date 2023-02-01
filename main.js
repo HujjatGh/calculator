2400dee60a895c990a61f13da5c215333aa646f8
HujjatGh <hujjat.pg@gmail.com>
2023-02-01 21:11:53 +0100

    calculator created

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const maximumAge= 100;
 let potatoesPerWeek= 30;
 let numberOfWeeks = (maximumAge - currentAge)*365/7;
 
+
 let totalPotatoes = numberOfWeeks * potatoesPerWeek;
 
-console.log(`Arman has  ${maximumAge - currentAge} and will eat ${totalPotatoes} until age of ${maximumAge}`);
+console.log(`Arman is  ${currentAge} years old and will eat ${totalPotatoes} until age of ${maximumAge}`);
