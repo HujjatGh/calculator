dabdc5480b6a189b29bf6744f6d0865cb507029b
HujjatGh <hujjat.pg@gmail.com>
2023-02-05 01:28:01 +0100

    three people challenge

--- main.js
+++ main.js
@@ -7,3 +7,5 @@ let numberOfWeeks = (maximumAge - currentAge)*365/7;
 let totalPotatoes = numberOfWeeks * potatoesPerWeek;
 
 console.log(`Arman is  ${currentAge} years old and will eat ${totalPotatoes} until age of ${maximumAge}`);
+
+
---
+++ main1.js
@@ -0,0 +1,6 @@
+let alex = {name: Alex, currentAge: 62, maximumAge: 100, potatoesPerWeek: 4}
+let david = {name: David, currentAge: 35, maximumAge: 100, potatoesPerWeek: 12}
+let michael = {name: Michael, currentAge: 41, maximumAge: 100, potatoesPerWeek: 1}
+
+
+console.log(alex)(0)
